6727cf17b7202c9f7a1ce0f826f81fce479e6f42
Osama-Belal <96381290+Osama-Belal@users.noreply.github.com>
2021-12-19 22:32:52 +0200

    Update players.c

--- players.c
+++ players.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int Mode(){
+  int mode;
+  printf("Choose mode:\nPlayer VS Player: 1");
+  printf("\nPlayer VS Computer: 2");
+  printf("\nFor multiple players (maximum 4 players): 3");
+  scanf("%d", &mode);
+  
+  while(mode != 1 || mode != 2 || mode != 3){
+    printf("Enter a valid number that represents meaningful mode :)");
+    scanf("%c", &mode);
+  }
+  return mode;
+}
+
+
+int main() {
+  
+  int mode = Mode();
+  
+    return 0;
+}
