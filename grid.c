dd5b73d8ef587c2b7f536da835d9008861790c80
Osama-Belal <96381290+Osama-Belal@users.noreply.github.com>
2021-12-20 15:44:10 +0200

    Update grid.c

--- grid.c
+++ grid.c
@@ -1,11 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void red(){printf("\033[1;31m");}
+void yellow(){printf("\033[1;33m");}
+void blue(){printf("\033[0;34m");}
+void resetColor(){printf("\033[0m");}
+
+
+void printGrid(int n, int m){
+
+  for(int i = 0;i < n+1; i++){
+
+      for(int j = 0; j < m+1; j++){
+        printf("🞇");
+        if(j < m){
+        printf("  ― ");}
+      }
+
+    printf("\n");
+
+    if(i < n){
+      for(int j = 0; j < m+1; j++){
+      printf(" ▏  ");
+      }
+    }
+    
+
+    printf("\n");
+
+  }
+
+}
+
 char Mode(){
   char mode;
+  yellow();
   printf("Choose mode \'b\' for beginner and \'e\' for expert");
   printf("\nAlso type \'c\' for choosing your special grid ");
   scanf("%c", &mode);
+  resetColor();
+
+  
+  while(mode != 'b' && mode != 'c' && mode != 'e'){
+    red();
+    printf("Please enter a valid charachter that represents meaningful mode :)\n");
+    scanf("%c", &mode);
+    resetColor();
+  }
+
   
   return mode;
 }
@@ -64,14 +106,27 @@ int main() {
   
   // corners
   grid[0][0] = 2;grid[0][m] = 2;grid[n][0] = 2;grid[n][m] = 2;
+
+
   
+  // columns rank before the grid
+  blue();printf("    ");
+  for(int j = 0; j < m+1; j++){printf("%d. ", j+1);}
+  printf("\n");
+  resetColor();
 
   for(int i = 0;i < n+1; i++){
+
+    blue();
+    printf("%d.  ", i+1);  // rows rank left to the grid
+    resetColor();
+
     for(int j = 0; j < m+1; j++){
-      printf("%d ", grid[i][j]);
+      printf("%d  ", grid[i][j]);
     }
     printf("\n");
   }
   
     return 0;
 }
+
